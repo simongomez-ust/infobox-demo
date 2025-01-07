890fd7e7a563c8a5da885b597bd34841f9661b2a
Simón Gómez Jiménez <simon.gomez@ust.com>
2025-01-07 12:57:31 +0000

    implemented connection to server for uploading leaves

--- app/my_office/webapp/controller/LeaveApp.controller.js
+++ app/my_office/webapp/controller/LeaveApp.controller.js
@@ -1,24 +1,59 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",    
-    "sap/ui/model/odata/v4/ODataModel"
-], (Controller, ODataModel) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/odata/v4/ODataModel",
+    "sap/ui/core/routing/History",
+    "sap/ui/core/UIComponent"
+], (Controller, ODataModel, History, UIComponent) => {
     "use strict";
 
 
-
     return Controller.extend("sap.demo.infobox.myoffice.controller.LeaveApp", {
+
         onInit() {
-            const oEmployeeModel = new ODataModel({
+            const oModel = new ODataModel({
                 serviceUrl: "/odata/v4/leave/"
             });
 
-            this.getView().setModel(oEmployeeModel, "Employee");
+            this.getView().setModel(oModel, "Employee");
 
             this.getView().bindElement({
                 path: "/Employee('1')",
                 model: "Employee"
             });
 
+        },
+        getRouter: function () {
+            return UIComponent.getRouterFor(this);
+        },
+        onConfirm: function () {
+            const oBindList = this.getView().getModel().bindList("/Leaves", undefined, undefined, undefined, undefined);
+            const oView = this.getView();
+            const startDate = oView.byId('DRS1').getDateValue();
+            const endDate = oView.byId('DRS1').getSecondDateValue();
+            const type = oView.byId('select').getSelectedKey();
+            const oDateFormat = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "yyyy-MM-dd" });
+            const oContext = oBindList.create({
+                "employeeId_ID": "1f96fb5e-bece-4a6b-84b0-5f0634c7377b",
+                "startDate": oDateFormat.format(startDate),
+                "endDate": oDateFormat.format(endDate),
+                "type": type,
+                "status": "Pending"
+            }, true)
+
+
+
+        },
+
+        onBack: function () {
+            this.getRouter().navTo("RouteMyOffice");
+        },
+
+        handleChange: function (oEvent) {
+            console.log('prueba')
+            var oCalendar = oEvent.getSource();
+
+            let oSelectedDate = oCalendar.getSelectedDates()[0]
+            console.log(oSelectedDate)
         }
     });
 });
